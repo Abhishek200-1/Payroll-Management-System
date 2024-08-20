b62c8df27de9187ef2c04630c8b1a2086c31adde
Dwivedi <devweb910@gmail.com>
2024-08-20 20:08:17 +0530

    Profile

--- Dist/MainDashbord.php
+++ Dist/MainDashbord.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['AdminId'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,7 +28,7 @@ if (!isset($_SESSION['AdminId'])) {
         <nav>
             <ul>
                 <li>
-                    <a href="../Admin/AdminProfile.php" class="logo">
+                    <a href="../Admin/Profile.php" class="logo">
                         <img src="../src/Images/Abhishek.jpg" />
                         <span class="nav-item">Vishwakarma Abhishek</span>
                     </a>
