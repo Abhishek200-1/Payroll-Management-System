392bdbcc4fec3eb76808d29744c6eb3552697a71
Dwivedi <devweb910@gmail.com>
2024-08-20 12:42:01 +0530

    Profile module of admin panel completed

--- Admin/Master/Department/display-add-department.php
+++ Admin/Master/Department/display-add-department.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/81aa89284e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../src/css/department.css">
+    <link rel="stylesheet" href="../../../src/css/department.css">
 </head>
 <a href="../add-department.php"></a>
+
 <body>
     <div class="container-fluid">
         <section class="main">
             <div class="main-top">
                 <h1>Department</h1>
                 <li><a href="">
-                    <i class="fas fa-solid fa-sign-out-alt pe-2"></i>
-                    <h6>Logout</h6>
+                        <i class="fas fa-solid fa-sign-out-alt pe-2"></i>
+                        <h6>Logout</h6>
                     </a>
                 </li>
             </div>
@@ -26,7 +28,7 @@
                     <h4>Data Tables Department</h4>
                     <table class="table">
                         <thead>
-                            <tr> 
+                            <tr>
                                 <th>#</th>
                                 <th>Id</th>
                                 <th>Department Name</th>
@@ -35,18 +37,16 @@
                         </thead>
                         <tbody>
                             <?php
-                                $i = 1;
-                                include('../../Backend/Database/connection.php');
-                                $q="SELECT * FROM `tbldepartment`";
-                                $result = mysqli_query($conn, $q);
-                                if ($result)
-                                {
-                                    while ($row = mysqli_fetch_assoc($result)) 
-                                    {
-                                        $DepartmentId=$row['Id'];
-                                        $DepartmentName = $row['Name'];
-                                        echo
-                                        '<tr>
+                            $i = 1;
+                            include('../../../Backend/Database/connection.php');
+                            $q = "SELECT * FROM `tbldepartment`";
+                            $result = mysqli_query($conn, $q);
+                            if ($result) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $DepartmentId = $row['Id'];
+                                    $DepartmentName = $row['Name'];
+                                    echo
+                                    '<tr>
                                             <th>' . $i++ . '</th>
                                                 <td>' . $DepartmentId . '</td>
                                                 <td>' . $DepartmentName . '</td>
@@ -55,8 +55,8 @@
                                                 <button><i class="fa-solid fa-trash"></i></button>
                                             </td>
                                         </tr>';
-                                    }
                                 }
+                            }
                             ?>
                             <!-- <tr>
                                 <td>01</td>
@@ -69,7 +69,9 @@
                             </tr> -->
                         </tbody>
                     </table>
-                    <button class="btnAddDepartment" onclick="location.href='../add-department.php'"><i class="fa-solid fa-square-plus"></i><h6>Add New Department</h6></button>
+                    <button class="btnAddDepartment" onclick="location.href='../add-department.php'"><i class="fa-solid fa-square-plus"></i>
+                        <h6>Add New Department</h6>
+                    </button>
                 </div>
             </section>
         </section>
--- Admin/Master/Employee/add-employee.php
+++ Admin/Master/Employee/add-employee.php
@@ -1,58 +1,52 @@
 <?php
-            $pic_upload=0;
-            include("../../../Backend/Database/connection.php");
-            if (isset($_POST['btn'])) {
-                $Firstname = $_POST['Fname'];
-                $Lastname = $_POST['Lname'];
-                $image = time().$_FILES['Image']['name'];
-                if(move_uploaded_file($_FILES['Image']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].'/Abhishek/Payroll-Management-System/Images/employee_image/'.$image)){
-                    $target_file=$_SERVER['DOCUMENT_ROOT'].'/Abhishek/Payroll-Management-System/Images/employee_image/'.$image;
-                    $imageFileType=strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-                    $picname=basename($_FILES['Image']['name']);
-                    $photo=time().$picname;
-                    if($imageFileType !="jpg" && $imageFileType !="jpeg" && $imageFileType != "png" )
-                    {?>
-                        <script>
-                            alert("please upload image having extension .jpg/.jpeg/.png");
-                        </script>\
-                        <?php
-                    }
-                    else if($_FILES['image']['size']>20000000)
-                    {?>
-                        <script>
-                            alert("please image exceed the size of 2 MB");
-                        </script>\
-                        <?php 
-                    }
-                    else{
-                        $pic_upload=1;
-                    }
-                    if($pic_upload ==1){
-
-                    }
-
-                    
-                }
-                $Email = $_POST['mail'];
-                $Department = $_POST['depart'];
-                $Shift = $_POST['shift'];
-                $Pnumber = $_POST['PhoneNum'];
-                $Address = $_POST['address'];
-                $Dateofbirth = $_POST['DOB'];
-                $Dateofjoining = $_POST['DOJ'];
-                $Gender = $_POST['gender'];
+$pic_upload = 0;
+include("../../../Backend/Database/connection.php");
+if (isset($_POST['btn'])) {
+    $Firstname = $_POST['Fname'];
+    $Lastname = $_POST['Lname'];
+    $image = time() . $_FILES['Image']['name'];
+    if (move_uploaded_file($_FILES['Image']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/Abhishek/Payroll-Management-System/Images/employee_image/' . $image)) {
+        $target_file = $_SERVER['DOCUMENT_ROOT'] . '/Abhishek/Payroll-Management-System/Images/employee_image/' . $image;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        $picname = basename($_FILES['Image']['name']);
+        $photo = time() . $picname;
+        if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png") { ?>
+            <script>
+                alert("please upload image having extension .jpg/.jpeg/.png");
+            </script>\
+        <?php
+        } else if ($_FILES['image']['size'] > 20000000) { ?>
+            <script>
+                alert("please image exceed the size of 2 MB");
+            </script>\
+<?php
+        } else {
+            $pic_upload = 1;
+        }
+        if ($pic_upload == 1) {
+        }
+    }
+    $Email = $_POST['mail'];
+    $Department = $_POST['depart'];
+    $Shift = $_POST['shift'];
+    $Pnumber = $_POST['PhoneNum'];
+    $Address = $_POST['address'];
+    $Dateofbirth = $_POST['DOB'];
+    $Dateofjoining = $_POST['DOJ'];
+    $Gender = $_POST['gender'];
 
 
-                $q = "insert into `tbladdemployee` (First_Name,Last_Name,Image,Email,Department,Shift,Pnumber,Address,Date_of_birth,Date_of_joining,Gender) values ('$Firstname','$Lastname','$image','$Email','$Department','$Shift','$Pnumber','$Address','$Dateofbirth','$Dateofjoining','$Gender')";
-                $result = mysqli_query($conn, $q);
-                if ($result) {
-                    // echo "<script>alert('Record Inserted Successfully');</script>";
-                    header('location:../../../Admin/Master/Employee/display-add-employee.php');
-                }
-            }
-        ?>
+    $q = "insert into `tbladdemployee` (First_Name,Last_Name,Image,Email,Department,Shift,Pnumber,Address,Date_of_birth,Date_of_joining,Gender) values ('$Firstname','$Lastname','$image','$Email','$Department','$Shift','$Pnumber','$Address','$Dateofbirth','$Dateofjoining','$Gender')";
+    $result = mysqli_query($conn, $q);
+    if ($result) {
+        // echo "<script>alert('Record Inserted Successfully');</script>";
+        header('location:../../../Admin/Master/Employee/display-add-employee.php');
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -61,6 +55,7 @@
     <script src="https://kit.fontawesome.com/81aa89284e.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <form action="" method="POST">
@@ -73,102 +68,99 @@
                         <input type="text" placeholder="Enter Your First Name" name="Fname" required>
                     </div>
 
-                <div class="input-box">
-                    <label for="name">Last Name</label>
-                    <input type="text" placeholder="Enter Your Last Name" name="Lname" required>
-                </div>
+                    <div class="input-box">
+                        <label for="name">Last Name</label>
+                        <input type="text" placeholder="Enter Your Last Name" name="Lname" required>
+                    </div>
 
-                <div class="input-box">
-                    <label for="name">Employee image</label>
-                    <input type="file" placeholder="Enter Your First Name" data-parsley-trigger="keyup" name="Image" class="form-control" required />
-                </div>
+                    <div class="input-box">
+                        <label for="name">Employee image</label>
+                        <input type="file" placeholder="Enter Your First Name" data-parsley-trigger="keyup" name="Image" class="form-control" required />
+                    </div>
 
-                <div class="input-box">
-                    <label for="Email">Email</label>
-                    <input type="text" placeholder="Enter Your Email" name="mail" required>
-                </div>
+                    <div class="input-box">
+                        <label for="Email">Email</label>
+                        <input type="text" placeholder="Enter Your Email" name="mail" required>
+                    </div>
 
-                <div class="input-box">
-                    <label for="depart">Department</label>
-                    <!-- <input type="text" placeholder="Enter Your Department" name="depart" required> -->
-                    <select class="option" name="depart">
-                        <option value="default">Select Department</option>
+                    <div class="input-box">
+                        <label for="depart">Department</label>
+                        <!-- <input type="text" placeholder="Enter Your Department" name="depart" required> -->
+                        <select class="option" name="depart">
+                            <option value="default">Select Department</option>
                             <?php
-                                include("../Backend/Database/connection.php");
-                                $query = "SELECT Id, Name FROM `tbldepartment`";
-                                $result = mysqli_query($conn, $query);
+                            include("../Backend/Database/connection.php");
+                            $query = "SELECT Id, Name FROM `tbldepartment`";
+                            $result = mysqli_query($conn, $query);
 
-                                if ($result) 
-                                {
-                                    while ($row = mysqli_fetch_assoc($result)) 
-                                    {
-                                        ?>
-                                            <option value="<?php echo $row['Id'] ?>"><?php echo $row['Name'] ?></option>
-                                        <?php
-                                    }
-                                }
+                            if ($result) {
+                                while ($row = mysqli_fetch_assoc($result)) {
                             ?>
-                    </select>
-                </div>
-
-                <div class="input-box">
-                    <label for="addresss">Shift</label>
-                    <!-- <input type="text" placeholder="Enter Your Department" name="shift" required> -->
-                    <select class="option" name="shift">
-                        <option value="default">Select Shift</option>
+                                    <option value="<?php echo $row['Id'] ?>"><?php echo $row['Name'] ?></option>
                             <?php
-                                include("../Backend/Database/connection.php");
-                                $query = "SELECT Id, Shift_Name, Start_Time, End_Time FROM `tblshift`";
-                                $result = mysqli_query($conn, $query);
-
-                                if ($result) 
-                                {
-                                    while ($row = mysqli_fetch_assoc($result)) 
-                                    {
-                                        ?>
-                                            <option value="<?php echo $row['Id'] ?>"><?php echo $row['Shift_Name'] ?></option>
-                                        <?php
-                                    }
                                 }
+                            }
                             ?>
-                    </select>
-                </div>
+                        </select>
+                    </div>
 
-                <div class="input-box">
-                    <label for="PhoneNumber">Phone Number</label>
-                    <input type="text" placeholder="Enter Your Phone Number" name="PhoneNum" required>
-                </div>
+                    <div class="input-box">
+                        <label for="addresss">Shift</label>
+                        <!-- <input type="text" placeholder="Enter Your Department" name="shift" required> -->
+                        <select class="option" name="shift">
+                            <option value="default">Select Shift</option>
+                            <?php
+                            include("../Backend/Database/connection.php");
+                            $query = "SELECT Id, Shift_Name, Start_Time, End_Time FROM `tblshift`";
+                            $result = mysqli_query($conn, $query);
 
-                <div class="input-box">
-                    <label for="addresss">Address</label>
-                    <input type="text" placeholder="Enter Your Address" name="address" required>
-                </div>
+                            if ($result) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                                    <option value="<?php echo $row['Id'] ?>"><?php echo $row['Shift_Name'] ?></option>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </select>
+                    </div>
 
-                <div class="input-box">
-                    <label for="Dob">Date Of Birth</label>
-                    <input type="date" placeholder="Enter Your Date Of Birth" name="DOB" required>
-                </div>
+                    <div class="input-box">
+                        <label for="PhoneNumber">Phone Number</label>
+                        <input type="text" placeholder="Enter Your Phone Number" name="PhoneNum" required>
+                    </div>
 
-                <div class="input-box">
-                    <label for="Dob">Date Of Joining</label>
-                    <input type="date" placeholder="Enter Your Date Of Birth" name="DOJ" required>
-                </div>
+                    <div class="input-box">
+                        <label for="addresss">Address</label>
+                        <input type="text" placeholder="Enter Your Address" name="address" required>
+                    </div>
 
-                <span class="gender-title">Gender</span>
-                <div class="gender-category">
-                    <input type="radio" name="gender" id="Male">
-                    <label>Male</label>
-                    <input type="radio" name="gender" id="Female">
-                    <labe>Female</labe>
-                    <input type="radio" name="gender" id="Other">
-                    <label>Other</label>
+                    <div class="input-box">
+                        <label for="Dob">Date Of Birth</label>
+                        <input type="date" placeholder="Enter Your Date Of Birth" name="DOB" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="Dob">Date Of Joining</label>
+                        <input type="date" placeholder="Enter Your Date Of Birth" name="DOJ" required>
+                    </div>
+
+                    <span class="gender-title">Gender</span>
+                    <div class="gender-category">
+                        <input type="radio" name="gender" id="Male">
+                        <label>Male</label>
+                        <input type="radio" name="gender" id="Female">
+                        <labe>Female</labe>
+                        <input type="radio" name="gender" id="Other">
+                        <label>Other</label>
+                    </div>
                 </div>
-            </div>
-            <div class="button-container">
-                <button type="submit" name="btn"><i class="fa-solid fa-square-plus"></i> Add New Employee</button>
-            </div>
+                <div class="button-container">
+                    <button type="submit" name="btn"><i class="fa-solid fa-square-plus"></i> Add New Employee</button>
+                </div>
             </div>
         </form>
     </div>
 </body>
+
 </html>
