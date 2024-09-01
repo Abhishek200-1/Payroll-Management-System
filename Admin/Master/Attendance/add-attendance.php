95daf921fd0343a07995de0d1b3322daaf3e1793
Dwivedi <devweb910@gmail.com>
2024-09-01 21:37:16 +0530

    Attendance module completed

--- Admin/Master/Attendance/add-attendance.php
+++ Admin/Master/Attendance/add-attendance.php
@@ -18,45 +18,44 @@
                 <div class="input-box">
                     <label for="name">Emp Id</label>
                     <input type="text" placeholder="Enter Emp Id" name="Emp_Id" id="empId" required>
-
                 </div>
-
                 <div class="input-box">
                     <label for="name">First Name</label>
                     <input type="text" placeholder="Enter Your First Name" name="firstName" id="firstName" required>
                 </div>
-
                 <div class="input-box">
                     <label for="username">Last Name</label>
                     <input type="text" placeholder="Enter Your Last Name" name="lastName" id="lastName" required>
                 </div>
-
                 <div class="input-box">
                     <label for="username">In Time</label>
                     <input type="time" placeholder="Enter Employee's In-Time" name="inTime" id="inTime" required>
                 </div>
-
-                <div class="input-box">
+                <!-- <div class="input-box">
                     <label for="Email">Out Time</label>
                     <input type="time" placeholder="Enter Your Employee's Out-Time" name="outTime" id="outTime" required>
+                </div> -->
+                <div class="input-box">
+                    <label for="isPresent">Is Present</label>
+                    <select name="isPresent" id="isPresent">
+                        <option value="present">Present</option>
+                        <option value="absent">Absent</option>
+                    </select>
                 </div>
-
                 <div class="input-box">
                     <label for="Email">Created On</label>
                     <input type="date" placeholder="Enter Created Date" name="createdOn" id="createDate" required>
                 </div>
-
-                <div class="gender-details">
+                <!-- <div class="gender-details">
                     <span class="gender-title">Work Time</span>
                     <div class="gender-category">
-                        <input type="radio" name="workTime" value="halfDay">
-                        <span>Half Day</span>
-                        <input type="radio" name="workTime" value="fullDay">
-                        <span>Full Day</span>
+                        <input type="radio" id="fullDay" name="workTime" value="fullDay">
+                        <label for="fullDay">Full Day</label>
+                        <input type="radio" id="halfDay" name="workTime" value="halfDay">
+                        <label for="halfDay">Half Day</label>
                     </div>
-                </div>
+                </div> -->
             </div>
-
             <div class="button-container">
                 <button type="submit" class="btn" name="addAttendance">Add Attendance</button>
             </div>
@@ -64,25 +63,27 @@
     </div>
     <?php
     if (isset($_POST['addAttendance'])) {
-        include("../Backend/Database/connection.php");
+        include("../../../Backend/Database/connection.php");
         $empId = $_POST['Emp_Id'];
         $firstName = $_POST['firstName'];
         $lastName = $_POST['lastName'];
         $inTime = $_POST['inTime'];
-        $outTime = $_POST['outTime'];
+        // $outTime = $_POST['outTime'];
+        $isPresent = $_POST['isPresent'];
+        echo $isPresent;
         $createdOn = $_POST['createdOn'];
-        $workTime = '';
-        if (isset($_POST['workTime'])  == 'halfDay') {
-            $workTime = 'HalfDay';
-        } else if (isset($_POST['workTime']) == 'fullDay') {
-            $workTime = 'FullDay';
-        }
+        // $workTime = '';
+        // if (isset($_POST['workTime'])  == 'halfDay') {
+        //     $workTime = 'HalfDay';
+        // } else if (isset($_POST['workTime']) == 'fullDay') {
+        //     $workTime = 'FullDay';
+        // }
 
-        $query = "INSERT INTO `tbladdattendance`(Emp_Id, First_Name, Last_Name, In_Time, Out_Time, Created_On, Is_Present, Work_Time) VALUES( '$empId', '$firstName', '$lastName', '$inTime', '$outTime', '$createdOn','present', '$workTime'); ";
+        $query = "INSERT INTO `tbladdattendance`(Emp_Id, First_Name, Last_Name, In_Time, Created_On, Is_Present) VALUES( '$empId', '$firstName', '$lastName', '$inTime', '$createdOn','present'); ";
         $result = mysqli_query($conn, $query);
         if ($result) {
-            //echo "Attendance Added!!";
-            header('location:../Admin/Display/display-add-attendance.php');
+            echo "Attendance Added!!";
+            header('location:./display-add-attendance.php');
         } else {
             echo "Error found : " . mysqli_error($conn);
         }
